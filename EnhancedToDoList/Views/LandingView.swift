bf8cece28b24b64118e3239fc3b45065e9c18fd8
Quinlan Taylor <qtaylor@lcs.on.ca>
2024-02-09 00:16:40 -0500

    start

---
+++ EnhancedToDoList/Views/LandingView.swift
@@ -0,0 +1,18 @@
+//
+//  LandingView.swift
+//  EnhancedToDoList
+//
+//  Created by Quin Taylor on 2024-02-09.
+//
+
+import SwiftUI
+
+struct LandingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandingView()
+}
