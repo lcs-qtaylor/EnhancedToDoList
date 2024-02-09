ba07a52c12575278f1065edf0e52f1c8c4721241
Quinlan Taylor <qtaylor@lcs.on.ca>
2024-02-09 00:15:08 -0500

    Initial Commit

---
+++ EnhancedToDoList/EnhancedToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnhancedToDoListApp.swift
+//  EnhancedToDoList
+//
+//  Created by Quin Taylor on 2024-02-09.
+//
+
+import SwiftUI
+
+@main
+struct EnhancedToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
